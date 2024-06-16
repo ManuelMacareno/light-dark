37837a82e57508a4dbc3ba653148da647bb8d527
Manuel Macareno <manu.macareno@gmail.com>
2024-06-16 08:42:52 -0300

    first commit

---
+++ components/light-dark/index.jsx
@@ -0,0 +1,21 @@
+import useLocalStorage from "./useLocalStorage"
+import './theme.css'
+
+export default function LightDarkMode() {
+
+    const [theme, setTheme] = useLocalStorage('theme', 'dark')
+
+    function handleToogleTheme() {
+        setTheme(theme === 'light' ? 'dark' : 'light')
+    }
+
+    return (
+        <div className="light-dark-mode" data-theme={theme}>
+            <div className="container">
+                <p>Hello World</p>
+                <button onClick={handleToogleTheme}>Change Theme</button>
+            </div>
+        </div>
+    )
+
+}
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import LightDarkMode from '../components/light-dark'
+
+function App() {
+
+  return (
+    <div>
+      <LightDarkMode />
+    </div>
+  )
+}
+
+export default App
